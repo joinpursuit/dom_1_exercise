790c5b749872d9b7bf718dfdd0c90647ffd14586
Muna Sharma <munasharma@pursuit.org>
2018-11-07 17:56:53 -0500

    dom.hw

---
+++ exc1.js
@@ -0,0 +1,6 @@
+let input = window.prompt("share a string");
+let x = window.prompt("now, pick a number");
+//let x = input.parseInt();
+for (let i = 0; i < x; i++) {
+  window.alert(input);
+}
---
+++ exc4.js
@@ -0,0 +1,20 @@
+let aKeysKee = document.getElementById("header");
+
+aKeysKee.innerText = window.prompt(
+  "Hey Hey! Welcome back! Sorry I forgot your name, may you remind me? "
+);
+window.alert("Thanks! You the real MVP");
+let first = document.getElementById("first");
+first.innerText = window.prompt("Okay Cutiepie, What is your first key?");
+let second = document.getElementById("second");
+second.innerText = window.prompt("And your second key?");
+let third = document.getElementById("third");
+third.innerText = window.prompt("ooo, okay I see! So what is your third key?");
+let fourth = document.getElementById("fourth");
+fourth.innerText = window.prompt(
+  "No way! That's my third key too! What's your fourth key?"
+);
+let fifth = document.getElementById("fifth");
+fifth.innerText = window.prompt(
+  "I hate to ask  about keys again, but what is your fifth and final key?"
+);
