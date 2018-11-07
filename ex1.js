b53bccba144956ab16d6b753eea835e6f08073c7
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2018-11-07 17:44:15 -0500

    Evgeniya Ezhova

---
+++ ex1.js
@@ -0,0 +1,9 @@
+let str = window.prompt("Hi! Write me something, please:")
+let x = window.prompt("And a number:")
+let i = 0;
+while(i < +x){
+window.alert(str);
+i++;
+}
+
+//it took me over an hour to make this run properly
---
+++ ex2.js
@@ -0,0 +1,9 @@
+let num = window.prompt("Give me a number:")
+let nmbr = +num;
+if(nmbr % 2 === 0){
+  alert("that number is even");
+} else if( nmbr % 2 === 1){
+  alert("that number is odd");
+} else {
+  alert("that is not a number/not a whole number :)")
+}
