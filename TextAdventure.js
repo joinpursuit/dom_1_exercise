439b5ece6869eb07bd5a83c0825d7882eceee709
Nicolle Loayza <n.loayza@gmail.com>
2018-11-07 16:04:43 -0500

    Nicolle Loayza

---
+++ TextAdventure.js
@@ -0,0 +1,14 @@
+
+window.alert("It's adventure time!")
+let input = window.prompt("Please enter your route: n, s, e, w")
+if(input === "n"){
+  alert("Hey! Look it's Santa! Merry Christmas bitches!")
+}else if(input === "s"){
+  alert("Oh no! You've stepped into the fires of hell!")
+}else if(input === "e") {
+  alert("Welcome to NY! Did that bum just flash you?")
+}else if(input === "w") {
+  alert("Welcome to Cali! I got the fattest blunt ready for you!")
+}else{
+  window.alert("Please refresh and pick valid choice")
+}
---
+++ stringRepeater.js
@@ -0,0 +1,10 @@
+let input =  window.prompt("Enter string")
+let number = window.prompt("Enter number")
+
+num = Number(num);
+
+for(let i = 0; i < num; i++){
+  window.alert(input)
+}
+
+// window.alert(input.repeat(number))
