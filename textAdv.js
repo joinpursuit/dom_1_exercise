80ec1e9cbf61f0d59b106142f8b773dac5363273
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-07 14:55:33 -0500

    third commit

---
+++ textAdv.js
@@ -0,0 +1,13 @@
+alert("There is a man with very special skills. \nHe will come for you.")
+
+let input = prompt("Will you: \n 'a': Run \n 'b': Make friends \n 'c': Give up")
+
+if (input === "a") {
+  alert("But can you run fast enough?")
+} else if (input === "b") {
+  alert("I hope one of them is Yo Mama!")
+} else if (input === "c") {
+  alert("Come on, punk. \nDon't just go gentle into the night (like a punk), \nRage, rage against the dying of the light")
+} else {
+  alert("Hey, man. Pick 'a', 'b', or 'c'. \nAt this point, there's no better options left.")
+}
