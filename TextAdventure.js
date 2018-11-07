1c205c3f85cc60dc11d3c0c2ce0036e393d4aba6
Andres.C <andrescabrera@pursuit.org>
2018-11-07 13:45:57 -0500

    keep going

---
+++ TextAdventure.js
@@ -0,0 +1,13 @@
+let string = window.prompt("You have 60 seconds to live and you've chosen with you dying minute to go only in one direction. ANNNNDDD Chooose -> N,S,W,E")
+string = string.toUpperCase();
+if (string === "N"){
+  window.alert("Attacked by White Walkers, Jon Snow didn't see it coming. DED. REMEMBER DA NORRFF");
+} else if (string === "W"){
+  window.alert("DED.Everyone knows that East is way better than West");
+} else if (string === "E") {
+window.alert(string === "Ded. Your were smited by the holy smiter.Womp womp ");
+} else if (string === "S"){;
+  window.alert("You ran straight into a huge wall. Silly Humans. Also DED.")
+} else {;
+  window.alert("Game Over. snake snake SNNNAAAAAkkkeeee")
+}
