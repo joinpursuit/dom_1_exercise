ddadb2d4138478326f9161ce7fbd4b994db812da
Rinat Tr <rinattregerman@pursuit.org>
2018-11-07 15:28:30 -0500

    added assignment 1,2,3

---
+++ script1.js
@@ -0,0 +1,12 @@
+let str = window.prompt("Gimme a good word:")
+let x = window.prompt("Gimme your lucky number:")
+
+x = parseInt(x);
+
+// let newStr = "";
+// for (let i = 0; i < x; i++) {
+//   newStr += str+" ";
+// }
+for (let i = 0; i < x; i++) {
+  window.alert(str);
+}
