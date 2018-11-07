5e811dabd70cb0c8cee6f750fdb02d820adc8056
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-07 17:51:37 -0500

    sixth commit

--- majorKeys.js
+++ majorKeys.js
@@ -16,22 +16,52 @@ document.getElementById("item3").innerText = third
 document.getElementById("item4").innerText = fourth
 document.getElementById("item5").innerText = fifth
 
-let array = [first, second, third, fourth, fifth]
-let arrayID = [item1, item2, item3, item4, item5]
+//BONUS:---------------------
 
-// for (let i = 0; i < array.length; i++) {
-//   for (let j = 0; j < arrayID.length; j++) {
-//     if (array[i] === array[i].toUpperCase()) {
-//       document.getElementById("arrayID[j]").style.backgroundColor = "yellow";
-//       document.getElementById("arrayID[j]").style.case = style.toLowerCase();
-//       document.getElementById("arrayID[j]").style.color = "red";
-//     }
-//   }
-// }
-
-if (first = first.toUpperCase) {
+if (first === first.toUpperCase()) {
   document.getElementById("item1").style.backgroundColor = "yellow";
+  first.toLowerCase();
+
+  document.getElementById("item1").style.color = "blue";
+}
+
+if (second === second.toUpperCase()) {
+  document.getElementById("item2").style.backgroundColor = "blue";
+  second.toLowerCase();
+  document.getElementById("item2").style.color = "pink";
+}
+
+if (third === third.toUpperCase()) {
+  document.getElementById("item3").style.backgroundColor = "lightgreen";
+  second.toLowerCase();
+  document.getElementById("item3").style.color = "red";
+}
+
+if (fourth === fourth.toUpperCase()) {
+  document.getElementById("item4").style.backgroundColor = "violet";
+  second.toLowerCase();
+  document.getElementById("item4").style.color = "white";
+}
+
+if (fifth === fifth.toUpperCase()) {
+  document.getElementById("item5").style.backgroundColor = "silver";
+  second.toLowerCase();
+  document.getElementById("item5").style.color = "gold";
 }
 
 
 // document.getElementById("item1").style.property = new style;
+
+// for loop try:----------------------
+// let array = [first, second, third, fourth, fifth]
+// let arrayID = [item1, item2, item3, item4, item5]
+//
+// for (let i = 0; i < array.length; i++) {
+//   for (let j = 0; j < arrayID.length; j++) {
+//     if (array[i] === array[i].toUpperCase()) {
+//       document.getElementById(arrayID[j]).style.backgroundColor = "yellow";
+//       array[i].toLowerCase();
+//       document.getElementById(arrayID[j]).style.color = "red";
+//     }
+//   }
+// }
