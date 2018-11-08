9ba2570deb936caecbffcf22bb2329dbcffac4e2
DianaG <dianagaona@pursuit.org>
2018-11-07 20:18:40 -0500

    Diana Gaona

---
+++ test.js
@@ -0,0 +1,47 @@
+// let str = window.prompt("Please enter a string");
+// let x = window.prompt("Enter a number");
+// let i = 0;
+// while (i < x) {
+//   window.alert(str);
+//   i++;
+// }
+
+// let num = window.prompt("Pick a number, I'll tell you if it's even or odd");
+// if (isNaN(num)) {
+//   window.alert(
+//     "Hmm.. Seems like you don't know your numbers because " +
+//       num +
+//       ' is not even a number. Think about your life choices and pick a real number'
+//   );
+// } else if (num % 2 === 0) {
+//   window.alert(num + ' is even');
+// } else {
+//   window.alert('Something is odd here... oh yeah!' + num + ' is quite odd.');
+// }
+
+// window.alert(
+//   `It's 3 a.m. and you are walking home but you see someone following you.`
+// );
+//
+// let input = window.prompt(
+//   `What do you do? \n F - Fight for your life \n S - Scream for help \n N - Nothing, you are just being paranoid`
+// );
+//
+// if (input === "f".toLowerCase()) {
+//   window.aler("Chill~ it was just a stray cat");
+// } else if (input === "s".toLowerCase()) {
+//   window.alert("Great... You woke up the crazy lady down the block..");
+// } else if (input === "n".toLowerCase()) {
+//   window.alert("Don't question why you get robbed all the time...");
+// } else {
+//   window.alert("Invalid key. Try again");
+// }
+
+let h1 = window.prompt("What is your name?");
+let header = document.getElementById("header");
+header.innerHTML = "<h1>Major Keys</h1>";
+
+// let welcome = document.getElementById("welcome");
+// debugger;
+// welcome.innerHTML = "<h1> I am in control now! </h1>";
+// console.log("after debugger");
