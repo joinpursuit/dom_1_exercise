11fc05a447eb97ae34dae7cbcd7dea9ec2bea420
Andrew KIl <kil.andrew@gmail.com>
2018-11-08 11:00:23 -0500

    solve assignment

--- final-exercise.js
+++ final-exercise.js
@@ -1,3 +1,23 @@
 let name = window.prompt("What is your name?");
 let key1 = window.prompt("What is your first key?");
-let key2 = 
+let key2 = window.prompt("What is your second key?");
+let key3 = window.prompt("What is your third key?");
+let key4 = window.prompt("What is your fourth key?");
+let key5 = window.prompt("What is your fifth key?");
+
+// document.getElementById('header').innerText = name;
+//
+// document.getElementById(button_id).innerHTML = 'Lock';
+
+let username = document.getElementById("header");
+header.innerText = name;
+let firstKey = document.getElementById("item1");
+item1.innerText = key1;
+let secondKey = document.getElementById("item2");
+item2.innerText = key2;
+let thirdKey = document.getElementById("item3");
+item3.innerText = key3;
+let fourthKey = document.getElementById("item4");
+item4.innerText = key4;
+let fifthKey = document.getElementById("item5");
+item5.innerText = key5;
