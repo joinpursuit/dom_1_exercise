6cb7e9844bcc3c1dd5a024051e74e6c45e464351
missivylady <crispinamuriel@pursuit.org>
2018-11-08 15:08:27 -0500

    Crispina Muriel

--- index3.js
+++ index3.js
@@ -1,12 +1,6 @@
-let alert = window.alert("‼🆘 A wild Corey appears... 🆘‼\n \n👱\n👕\n👖\n👟👟");
-let prompt = window.prompt("What do you do...⁉\n\nUse Pokeball 🔮 (P)\nSnap a picture 📷 (S)\nRun away 🏃 (R)");
-
-if (prompt.toUpperCase() === 'P') {
-  window.alert('��\n... \n... \n...\n \nYou have caught Corey...! ��');
-} else if (prompt.toUpperCase() === 'S') {
-  window.alert('You take out your camera and snap a picture of Corey...\nTime to sell it on Ebay!\nGood luck selling it though ��')
-} else if (prompt.toUpperCase() === 'R') {
-  window.alert('You start to run away... but you turn around and see Corey furiously sprinting after you...! ��\n�� *BAM* �� \n�� *POW* �� \n�� *KABOOM* �� \nYou have been beaten up by Corey. ��')
-} else {
-  window.alert('Please choose a valid input!')
-}
+let name = window.prompt("What is your name?");
+let key1 = window.prompt("What is your first key?");
+let key2 = window.prompt("What is your second key?");
+let key3 = window.prompt("What is your third key?");
+let key4 = window.prompt("What is your fourth key?");
+let key5 = window.prompt("What is your fifth key?");
