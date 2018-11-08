c36761577eb68da757c9819aa60c7e59236a10d2
godsby <gg4biz@gmail.com>
2018-11-07 23:49:47 -0500

    Huandan Lu

--- final.js
+++ final.js
@@ -3,5 +3,17 @@ let header = document.getElementById("header");
 let input = window.prompt("What's your name?");
 header.innerHTML = input;
 
-// let item1 = document.getElementById("item1");
-// item1.innerHTML = window.prompt("What's your first key?");
+let item1 = document.getElementById("item1");
+item1.innerHTML = window.prompt("What's your first key?");
+
+let item2 = document.getElementById("item2");
+item2.innerHTML = window.prompt("What's your second key?");
+
+let item3 = document.getElementById("item3");
+item3.innerHTML = window.prompt("What's your Three key?");
+
+let item4 = document.getElementById("item4");
+item4.innerHTML = window.prompt("What's your four key?");
+
+let item5 = document.getElementById("item5");
+item5.innerHTML = window.prompt("What's your fifth key?");
