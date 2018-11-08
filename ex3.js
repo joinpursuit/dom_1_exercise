da8ae322353290525b52b49d9c73476864fad6fc
Erick <erickarellano@pursuit.org>
2018-11-07 23:46:37 -0500

    Erick Arellano

---
+++ ex3.js
@@ -0,0 +1,14 @@
+// window.alert("Which season do you prefer?")
+// let string = window.prompt("fall, winter, summer, spring?")
+// if (string.toLowerCase() == "fall"){
+//   window.alert("Leaves will fall upon you.")
+// }
+// if (string.toLowerCase() == "spring"){
+//   window.alert("Let it rain.")
+// }
+// if (string.toLowerCase() == "summer"){
+//   window.alert("Let it melt your heart.")
+// }
+// if (string.toLowerCase() == "winter"){
+//   window.alert("Let us freeze.")
+// }
---
+++ ex4.js
@@ -0,0 +1,23 @@
+// let name = window.prompt("What's your name?");
+// let item1 = document.getElementById("header");
+// item1.innerHTML = name;
+//
+// let k1 = window.prompt("What's your first key?");
+// let kk1 = document.getElementById("item1");
+// kk1.innerHTML = k1;
+//
+// let k2 = window.prompt("What's your second key?");
+// let kk2 = document.getElementById("item2");
+// kk2.innerHTML = k2;
+//
+// let k3 = window.prompt("What's your third key?");
+// let kk3 = document.getElementById("item3");
+// kk3.innerHTML = k3;
+//
+// let k4 = window.prompt("What's your fourth?");
+// let kk4 = document.getElementById("item4");
+// kk4.innerHTML = k4;
+//
+// let k5 = window.prompt("What's your fifth key?");
+// let kk5 = document.getElementById("item5");
+// kk5.innerHTML = k5;
