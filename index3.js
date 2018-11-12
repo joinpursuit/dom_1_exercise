184855ad369d5b6a39cd695d20bb64e347dee74d
missivylady <crispinamuriel@pursuit.org>
2018-11-12 14:58:51 -0500

    crispina Muriel

--- index3.js
+++ index3.js
@@ -4,3 +4,11 @@ let key2 = window.prompt("What is your second key?");
 let key3 = window.prompt("What is your third key?");
 let key4 = window.prompt("What is your fourth key?");
 let key5 = window.prompt("What is your fifth key?");
+
+let header = document.getElementById('header');
+header.innerText = name
+item1.innerText = key1
+item2.innerText = key2
+item3.innerText = key3
+item4.innerText = key4
+item5.innerText = key5
